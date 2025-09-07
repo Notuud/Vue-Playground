5fe84278dcefb72ae0de9801c90400708f5edfe3
Notuud <paveljahoda1997@gmail.com>
2025-09-07 19:13:21 +0200

    Add buttons and global filtering to sessions DT

--- src/views/account/SessionsView.vue
+++ src/views/account/SessionsView.vue
@@ -11,10 +11,11 @@
         dataKey="id"
         filterDisplay="row"
         :loading="loading"
-        :globalFilterFields="['platform']"
+        :globalFilterFields="['session', 'token', 'ip', 'userAgent']"
         stripedRows
         sortField="date" 
         :sortOrder="-1"
+        :rowClass="rowClass"
     >
         <template #header>
             <div class="flex justify-end gap-4">
@@ -41,6 +42,11 @@
         </template>
         <template #loading>
             Loading data. Please wait.
+        </template>
+        <template #paginatorstart>
+            <Button severity="danger">
+                <FontAwesomeIcon :icon="['fas', 'trash-can']" /> Odhlásit všechny relace
+            </Button>
         </template>
         <Column
             field="session"
@@ -96,6 +102,25 @@
                 {{ data.userAgent.length > 36 ? `${data.userAgent.substring(0, 36)} ...` : data.userAgent }}
             </template>
         </Column>
+        <Column class="!text-end">
+            <template #body="{ data }">
+                <Tag 
+                    v-if="data.isCurrent"
+                    :value="'Aktuální'" 
+                    severity="info" 
+                    rounded
+                    class="!text-[12px] !bg-blue-200 dark:!bg-blue-950"
+                /> 
+                <Button 
+                    v-else
+                    severity="danger"
+                    size="small"
+                    @click="signOutSession(data)" 
+                >
+                    <FontAwesomeIcon :icon="['fas', 'trash-can']" />
+                </Button>
+            </template>
+        </Column>
     </DataTable>
 </template>
 
@@ -108,6 +133,7 @@ import InputText from 'primevue/inputtext'
 import IconField from 'primevue/iconfield'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
+import Tag from 'primevue/tag'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const loading = ref(true)
@@ -122,8 +148,7 @@ const filters = ref();
 
 const initFilters = () => {
     filters.value = {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        platform: { value: null, matchMode: FilterMatchMode.EQUALS },
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS }
     };
 };
 
@@ -140,4 +165,12 @@ const dtData = [
     { session: "9ct2*****f6kq", token: "c813*****70b6", loginDate: "07.09.2025 17:39", lastActivityDate: "07.09.2025 18:02", expirationDate: "07.10.2025 18:02", ip: "2a02:78a6:558:0:6501:2d80:2a2d:750b", userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36", isCurrent: true },
     { session: "cm77*****vja4", token: "c813*****70b6", loginDate: "28.08.2025 15:24", lastActivityDate: "04.09.2025 12:08", expirationDate: "04.10.2025 12:08", ip: "109.202.79.44", userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36", isCurrent: false },
 ]
+
+const rowClass = (data: any) => {
+    return [{ '!bg-blue-300 dark:!bg-blue-900': data.isCurrent }];
+};
+
+function signOutSession(rowData: any) {
+    console.log(rowData)
+}
 </script>
